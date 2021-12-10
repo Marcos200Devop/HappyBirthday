8ffa96da63e99fb0926238e0569b6c5fb88d7bac
Marcos200Devop <82180545+Marcos200Devop@users.noreply.github.com>
2021-12-09 20:00:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Ingresa tu nombre");
+ var b=prompt("Ingresa la fecha de cumpleaños");
+  	if(a== "natalia" && b=="12 de diciembre")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("DATOS MALOS, TU NO ERES NATALIA");
+  		window.location.href="index.html";
+  	}
+  	
